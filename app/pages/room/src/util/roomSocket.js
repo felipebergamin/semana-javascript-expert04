a3fec6aa39105eb5a5bff20799931830a7f66862
Felipe Bergamin <felipebergamin6@gmail.com>
2021-05-17 23:02:49 -0300

    aula 01

---
+++ app/pages/room/src/util/roomSocket.js
@@ -0,0 +1,20 @@
+import { constants } from "../../../_shared/constants.js";
+import SocketBuilder from "../../../_shared/sockerBuilder.js";
+
+export default class RoomSocketBuilder extends SocketBuilder {
+  constructor({ socketUrl, namespace }) {
+    super({ socketUrl, namespace });
+    this.onRoomUpdated = () => {};
+  }
+
+  setOnRoomUpdated(fn) {
+    this.onRoomUpdated = fn;
+    return this;
+  }
+
+  build() {
+    const socket = super.build();
+    socket.on(constants.events.LOBBY_UPDATED, this.onRoomUpdated);
+    return socket;
+  }
+}
---
+++ server/src/util/constants.js
@@ -0,0 +1,8 @@
+export const constants = {
+  events: {
+    LOBBY_UPDATED: "lobbyUpdated",
+    USER_CONNECTED: "userConnection",
+    USER_DISCONNECTED: "userDisconnection",
+    JOIN_ROOM: "joinRoom",
+  },
+};
